2b54eb305706564faf63bfd47cc3cc78808f15d6
amgaadezzat <amgaadezzat99@gmail.com>
2023-12-10 17:04:57 +0200

    create Tags Tables

---
+++ app/Repositories/Repository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Http\Interfaces\RepositoryInterface;
+
+class Repository implements RepositoryInterface
+{
+
+    public function all()
+    {
+        // TODO: Implement all() method.
+    }
+
+    public function create($data)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function update($data, $id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function destroy($id)
+    {
+        // TODO: Implement destroy() method.
+    }
+
+    public function show($id)
+    {
+        // TODO: Implement show() method.
+    }
+}
